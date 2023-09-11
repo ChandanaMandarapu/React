125b4f6c68f0dd6a8077d6747744a52792357562
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2023-09-11 21:34:00 +0530

    first react component with Vite React

---
+++ 1basicsvitereact/src/App.jsx
@@ -0,0 +1,14 @@
+import Hello from "./Basic"
+function App() {
+  const username = "Chandana Mandarapu"
+
+  return (
+    <>
+    <Hello/>
+    <h1>Hello! {username}</h1>
+    <h2>test Heading</h2>
+   </>
+  )
+}
+
+export default App
---
+++ 1basicsvitereact/src/Basic.jsx
@@ -0,0 +1,6 @@
+function Hello(){
+    return(
+        <h1>My First React App</h1>
+    )
+}
+export default Hello
