78ac8797ec61b450768f58ae0b076cc97aa2fab0
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2023-09-19 12:49:50 +0530

    Basics

--- 4basics/src/Content.jsx
+++ 4basics/src/Content.jsx
@@ -1,31 +1,61 @@
-
+import { useState } from "react";
 
 const Content = () => {
-    const handleNameChange = () => {
+  const [items] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "Almonds 1kg | Cashews 1kg",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Item 2",
+    },
+    {
+      id: 3,
+      checked: false,
+      item: "Item 3",
+    },
+  ]);
+
+  /* const handleNameChange = () => {
         const names = ["pallavi","karthik","kuldeep"];
         const int = Math.floor(Math.random() * 3)
-        return names[int]
+        setName(names[int]) 
     }
     const handleClick = () =>{
-      console.log("button is clicked")
+      setCount(count + 1)
+      setCount(count + 1)
+      console.log(count)
     }
-    const handleClick2 = (name) =>{
-      console.log(`${name} was clicked`)
-    }
-    const handleClick3 = (e) =>{
-      console.log(e.target.innerText)
-    }
+    const handleClick2 = () =>{
+      console.log(count)
+    } */
   return (
     <main>
-            <p onDoubleClick={handleClick}>
-            Hello {handleNameChange()}
+      {/* <p onDoubleClick={handleClick}>
+            Hello {name}
             </p>
+            <button onClick={handleNameChange}>Changed Name</button>
             <button onClick={handleClick}>Click it</button>
-            <button onClick={() => handleClick2('chandana')}>Click it</button>
-            <button onClick={(e) => handleClick3(e)}>Click it</button>
-            
+            <button onClick={handleClick2}>Click it</button>
+             */}
+             <ul>
+              setItems
+              {items.map((item) => (
+                <li className="item" key= {item.id}>
+                  <input 
+                  type="checbox" 
+                  checked = {item.checked}
+                  />
+                  <label>{item.item}</label>
+                  <button>Delete</button>
+                </li>
+              ))}
+             </ul>
     </main>
-  )
-}
+  );
+};
 
-export default Content
+export default Content;
