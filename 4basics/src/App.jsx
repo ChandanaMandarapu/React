f8ce662e3783386cc8a381b5f12498956daca3ce
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2023-09-20 13:58:20 +0530

    JSX | Components | Props

--- 4basics/src/App.jsx
+++ 4basics/src/App.jsx
@@ -1,16 +1,37 @@
-import Header from './Header'
-import Content from './Content'
-import Footer from './Footer'
+// import React from "react";
+import Add from "./components/Add";
+import Multiply from "./components/Multiply";
+import Lists from "./components/lists";
+import Props from "./components/Props";
+
 function App() {
   return (
     <>
-    <div className='App'>
-        <Header />
-        <Content />
-        <Footer />
-        </div>
+      <Add />
+      <Multiply />
+      <Lists />
+      <Props 
+        img = "https://lh3.googleusercontent.com/ogw/AKPQZvytz0mMMrmBzi4CXguLNDBCIWm3Kk6CdYTtNQJz=s32-c-mo"
+        name = "Chandana"
+        age = {19}
+        hobbies = {["coding","reading","Drawing"]}
+        >
+          <p>Lorem ipsum dolorlorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed veritatis quos, doloremque voluptatem pariatur alias? Consequatur quam omnis rem cum.</p>
+        </Props>
+      <section>
+        <h1>Hello Im learning React 💜</h1>
+        <p>This is a paragraph :)</p>
+        {/* closing tag */}
+        <img src="" alt="" />
+        <form>
+          <label htmlFor="name">Name</label>
+          <input type="text" />
+        </form>
+
+        
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
