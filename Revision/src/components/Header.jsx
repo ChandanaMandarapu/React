22d644705a0d78e709d6f1e559e2f28844b49284
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2023-10-17 17:49:56 +0530

    react

---
+++ Revision/src/components/Header.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+
+const Header = () => {
+  return (
+    <header style = {{
+      // backgroundColor : "black",
+      // color : "white",
+      // textAlign : "center",
+      // margin : "0",
+      // padding : "0"
+    }}>
+        <h1>Skills List</h1>
+    </header>
+  )
+}
+
+export default Header
