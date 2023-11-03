dc6221f4ea916f7babfa4f0a02a4502e28736e70
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2023-11-03 21:36:29 +0530

    props - reusability of components

---
+++ props-practice/src/App.jsx
@@ -0,0 +1,34 @@
+import "./App.css";
+import Contact from "./Contact";
+function App() {
+  return (
+    <div className="contacts">
+      <Contact
+        img="https://media.istockphoto.com/id/1308542628/photo/young-business-woman-stock-photo.webp?b=1&s=170667a&w=0&k=20&c=oQfuyDCgNm9IUlPP9humlj3R1HB22dDOH74GFgsiwZQ="
+        name="Mrs. Pallavi"
+        phone="1223045564"
+        email="pallavi@gmail.com"
+      />
+      <Contact
+        img="https://media.istockphoto.com/id/1288148699/photo/young-business-woman-in-office-stock-photo.webp?b=1&s=170667a&w=0&k=20&c=K-GKLOIEwGJVrZRC4WVG4yD4Fthl8GuaumMh_iYzVRo="
+        name="Mrs. Aditi"
+        phone="9876464759"
+        email="aditi@gmail.com"
+      />
+      <Contact
+        img="https://media.istockphoto.com/id/958064228/photo/portrait-of-happy-young-man.webp?b=1&s=170667a&w=0&k=20&c=ywaPLXj8meNw8tmZ3rOju5nrtlmhMwrQnjpW4nlqy_s="
+        name="Mr.Rishi"
+        phone="898989898"
+        email="rishi@gmail.com"
+      />
+      <Contact
+        img="https://media.istockphoto.com/id/1171169099/photo/man-with-crossed-arms-isolated-on-gray-background.webp?b=1&s=170667a&w=0&k=20&c=W5AFEjtmFjyyIGF40oSTOm6E0rj7AjF_zIIF-0zdVxk="
+        name="Mr.Rohit"
+        phone="1223045564"
+        email="rohit@gmail.com"
+      />
+    </div>
+  );
+}
+
+export default App;
