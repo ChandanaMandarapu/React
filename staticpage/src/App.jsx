ffd3087636041f865ed5c4cafc249e34e8917333
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2023-11-03 12:38:25 +0530

    static page done

---
+++ staticpage/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header"
+import Content from "./Content"
+import "./App.css"
+function App() {
+  return (
+    <>
+    <Header />
+    <Content />
+    </>
+  )
+}
+
+export default App
---
+++ staticpage/src/Header.jsx
@@ -0,0 +1,18 @@
+// import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <nav>
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRRECfdTqlVrG03aR41yIfxUuQb1IgLN-TzsQ&usqp=CAU"
+          alt="" className="nav--icon"
+        />
+        <h3 className="nav--logo_text">ReactFacts</h3>
+        <h4 className="nav--title">React Course - Project 1</h4>
+      </nav>
+    </>
+  );
+};
+
+export default Header;
