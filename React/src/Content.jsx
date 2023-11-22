f178a44840308ae07db1c70a568a798b1797e5f8
ChandanaMandarapu <chandanamandarapu93@gmail.com>
2023-11-22 15:15:33 +0530

    modified

--- React/src/Content.jsx
+++ React/src/Content.jsx
@@ -1,13 +1,27 @@
 // import React from 'react'
+import { useState } from "react"
 const Content = () => {
+    const [name,setName] = useState("chandu")
     const handleNameChange = () => {
         const names = ["krishna","ram","parvati"]
         const int = Math.floor(Math.random() * 3)
-        return names[int]
+        setName[names[int]]
+      }
+      const handleClick =() => {
+        console.log("clicked")
+      }
+      const handleClick2 =() => {
+        console.log(`${name} was clicked`)
+      }
+      const handleClick3 =(e) => {
+        console.log(e)
       }
   return (
     <main>
-        Hello {handleNameChange}
+        <p onClick={handleNameChange}>Hello {name}</p>
+        <button onClick={handleNameChange}>Click</button>
+        <button onClick={() => handleClick2('chandana')}>click it</button>
+        <button onClick={(e) => handleClick3(e)}>click</button>
     </main>
   )
 }
